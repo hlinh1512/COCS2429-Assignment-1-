aa4de9b2dbda9350de1668c68b102fa55439d32d
Hoang Linh <73870812+hlinh1512@users.noreply.github.com>
2020-12-11 19:53:38 +0700

    Add files via upload

--- a1_s3880313_nguyen_hoang_linh_p2.py
+++ a1_s3880313_nguyen_hoang_linh_p2.py
@@ -8,16 +8,12 @@
 
 #Problem 2 Assignment 1: Clock drawing and New Year count down
 
-import turtle, datetime, time
+import turtle, datetime, time, math
 
-
-def draw_circle():
-    t = turtle.Turtle()
 
 
 def draw_clock():
     t = turtle.Turtle()
-    t.speed(100)
     t.pensize(10)
 
 
@@ -42,8 +38,11 @@ def draw_clock():
         for k in range (0,4):
             t.right(360/60)
             t.forward(230)
-            t.shape("classic")
-            t.stamp()
+            t.pensize(3)
+            t.pendown()
+            t.backward(10)
+            t.forward(10)
+            t.penup()
             t.backward(230)
         t.right(360/60)
 
@@ -146,126 +145,119 @@ def draw_clock():
     t.right(7)
     t.setheading(0) #set angle back to west direction for next step.
 
+    t.hideturtle()
 
 
 #DRAW 3 HANDS OF THE CLOCK
-
-
-    # #hour hand
-    # t.pensize(10)
-    # t.setheading(90)
-    # t.pendown()
-    # t.backward(20)
-    # t.forward(130)
-    # t.shape("arrow")
-    # t.stamp()
-    # t.penup()
-    # t.goto(0,100) #back to center position
-
-    # #minute hand
-    # t.setheading(0)
-    # t.pendown()
-    # t.backward(20)
-    # t.forward(225)
-    # t.stamp()
-    # t.penup()
-    # t.goto(0,100) #back to center position
-    # t.pendown()
-
-    # (0,0) stamp a red circle center of the clock\
-    t.setheading(90)
-    t.penup()
-    t.backward(3)  # backward the turtle
-    t.pendown()
-    t.setheading(0)
-    t.pensize(4)
-    t.color("red")
-    t.fillcolor("red")
-    t.pendown()
-    t.begin_fill()
-    t.circle(5)
-    t.end_fill()
-    t.penup()
-    t.setheading(90)
-    t.forward(3)
-    t.setheading(0)
-    t.color("black")
-    t.pendown()
-
-    t.hideturtle()
-
-    # #second hand
-    # t.pensize(3)
-    # t.color("red")
-    # t.left(45)
-    # t.forward(210)
-    # t.stamp()
-
-
-#get current time
+#GET CURRENT TIME
     curr_time = time.localtime()
     curr_hour = curr_time.tm_hour
     curr_min = curr_time.tm_min
     curr_sec = curr_time.tm_sec
 
-    #turtle to draw the second hand
+    #turtle to draw the second-hand
     t_sec = turtle.Turtle() #create another turtle to draw second-hand.
-    t_sec.penup()
-    t_sec.goto(0,100) #set t1 to the center of the circle
-    t_sec.setheading(90) #set the second-hand to 0 second
-    t_sec.right(curr_sec * (360/60)) #set the second-hand direction to the current time second direction on the clock
-    t_sec.pendown()
+    t_sec.color("red")
+    t_sec.pensize(5)
+    t_sec.hideturtle()
+    tsec_length = 205
+    tsec_angle = curr_sec * 6 + 6 #angle of current second. plus 6 because use time sleep so plus 6 to equal current second
+
+    # turtle to draw the min-hand
+    t_min = turtle.Turtle()  # create turtle for min-hand
+    t_min.color("black")
+    t_min.pensize(8)
+    t_min.hideturtle()
+    tmin_length = 205
+    # tmin_angle = curr_min * (360 / 60)
+    tmin_angle = curr_min * (360/60) + curr_sec * (360/60/60) #angle of current minute. I plus the angle of current sec into cur min angle
 
 
-
-    #turtle to draw the minute hand
-    t_min = turtle.Turtle()
-    t_min.penup()
-    t_min.goto(0,100)
-    t_min.setheading(90)
-    t_min.right(curr_min * (360/60))
-    t_min.pendown()
+    #turtle to draw the hour-hand
+    t_hour = turtle.Turtle()
+    t_hour.color("black")
+    t_hour.pensize(8)
+    t_hour.hideturtle()
+    thour_length = 150
+    # thour_angle = curr_hour * (360 / 12)
+    thour_angle = curr_hour * (360/12) + curr_min * (360/60/12) + curr_sec * (360/60/60/12) #angle of current hour
 
 
     while True:
-        for k in range (0,60):
-            t_min.right(360/60)
-            t_min.pensize(8)
-            t_min.forward(205)
-            t_min.stamp()
-            t_min.penup()
-            t_min.backward(205)
-            t_min.pendown()
-            for j in range (0,60):
-               t_sec.right(360/60)
-               t_sec.pensize(3)
-               t_sec.color("red")
-               t_sec.forward(210)
-               t_sec.stamp()
-               t_sec.penup()
-               t_sec.backward(210)
-               t_sec.pendown()
-               t_sec.clear()
-               time.sleep(1) #delay the time to make it moves 1 second in real time
-               if (curr_sec == 0):
-                   break #dang het 1 vong ma no moi' cap nhat
-
-            t_min.clear()
+        #while loop to draw hour-hand
+        t_hour.clear()
+        t_hour.up()
+        t_hour.goto(0,100)
+        t_hour.down()
+        thour_x = math.sin(math.radians(thour_angle)) * thour_length
+        thour_y = math.cos(math.radians(thour_angle)) * thour_length + 100
+        t_hour.goto(thour_x, thour_y)
+        thour_angle += (360/60/60/12) #current hour it moves per second
 
 
 
 
+        # while loop to draw minute-hand
+        t_min.clear()
+        t_min.up()
+        t_min.goto(0, 100)
+        t_min.down()
+        tmin_x = math.sin(math.radians(tmin_angle)) * tmin_length
+        tmin_y = (math.cos(math.radians(tmin_angle)) * tmin_length) + 100
+        t_min.goto(tmin_x, tmin_y)
+        tmin_angle += (360/60/60) #current min it moves per second
 
+        #while loop to draw second-hand
+        t_sec.clear()
+        t_sec.up()
+        t_sec.goto(0, 100)  # set t_sec to the center of the circle
+        t_sec.down()
+        tsec_x = math.sin(math.radians(tsec_angle)) * tsec_length  # x position after 1 second
+        tsec_y = (math.cos(math.radians(tsec_angle)) * tsec_length) + 100 #y position after 1 second. I plus 100 because the starting coordinate is (0,100)
+        t_sec.goto(tsec_x, tsec_y)
+        tsec_angle += 6
+
+
+        time.sleep(1)
+        scr.update()
+
+
+
+
+
+def draw_string():
+    curr_date = datetime.date.today()
+    curr_time = time.localtime()
+    curr_hour = int(curr_time.tm_hour)
+    curr_min = int(curr_time.tm_min)
+    curr_sec = int(curr_time.tm_sec)
+
+    #calculate day, hour, min, sec left to new year
+    day_left = str(31 - curr_date.day) + " day, "
+    hour_left = str(24 - curr_hour) + " hour, "
+    min_left = str(60 - curr_min) + " minute, and "
+    sec_left = str(60 - curr_sec) + " second "
+    string = "Current time is " + str(curr_date) + ' ' + str(curr_hour) + ":" + str(curr_min) + ":" + str(
+        curr_sec) + ", \n there are " + day_left + hour_left + min_left + sec_left + "left to New Year "
+    #write the whole string
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    t.goto(-350,-250)
+    t.write(string, font=("Comic Sans MS", 17, "bold")) #draw the string
+    t.pendown()
 
 
 
 #main function
 if __name__ == "__main__":
     scr = turtle.Screen()
-    scr.setup(800,800)
-
-
+    scr.setup(1000, 800)
+    scr.tracer(0)
+    draw_string()
     draw_clock()
 
+
     scr.exitonclick()
+
 
